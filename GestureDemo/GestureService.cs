5ddbdfb5eac84c0d82220c230eb51ae4443d4753
zl <simple0812@sina.cn>
2016-02-18 14:19:12 +0800

    add gesture demo

---
+++ GestureDemo/GestureService.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.UI.Input;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Input;
+
+namespace GestureDemo
+{
+    public class GestureService
+    {
+        private GestureRecognizer _gestureRecognizer;
+
+        /*
+        * Windows.UI.Input.GestureSettings 是一个 flag 枚举，其值包括：
+        * None, Tap, DoubleTap, Hold, HoldWithMouse, RightTap, Drag, CrossSlide, ManipulationTranslateX, ManipulationTranslateY, ManipulationTranslateRailsX, ManipulationTranslateRailsY, ManipulationRotate, ManipulationScale, ManipulationTranslateInertia, ManipulationRotateInertia, ManipulationScaleInertia
+        */
+        public GestureRecognizer GtGestureListener(UIElement ele)
+        {
+            _gestureRecognizer = new GestureRecognizer();
+
+            ele.PointerMoved += GestureRecognizerDemo_PointerMoved;
+            ele.PointerPressed += GestureRecognizerDemo_PointerPressed;
+            ele.PointerReleased += GestureRecognizerDemo_PointerReleased;
+
+            return _gestureRecognizer;
+        }
+
+        private void GestureRecognizerDemo_PointerPressed(object sender, PointerRoutedEventArgs e)
+        {
+            // 告诉 GestureRecognizer 指针按下了，以便 GestureRecognizer 做手势监测
+            _gestureRecognizer.ProcessDownEvent(e.GetCurrentPoint(null));
+        }
+
+        private void GestureRecognizerDemo_PointerMoved(object sender, PointerRoutedEventArgs e)
+        {
+            // 告诉 GestureRecognizer 指针移动中，以便 GestureRecognizer 做手势监测
+            _gestureRecognizer.ProcessMoveEvents(e.GetIntermediatePoints(null));
+        }
+
+        private void GestureRecognizerDemo_PointerReleased(object sender, PointerRoutedEventArgs e)
+        {
+            // 告诉 GestureRecognizer 指针释放了，以便 GestureRecognizer 做手势监测
+            _gestureRecognizer.ProcessUpEvent(e.GetCurrentPoint(null));
+        }
+    }
+}
---
+++ GestureDemo/MainPage.xaml.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Input;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+//“空白页”项模板在 http://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409 上有介绍
+
+namespace GestureDemo
+{
+    /// <summary>
+    /// 可用于自身或导航至 Frame 内部的空白页。
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        private GestureRecognizer _gestureRecognizer;
+
+        public MainPage()
+        {
+            this.InitializeComponent();
+            _gestureRecognizer = new GestureService().GtGestureListener(this);
+
+            // 监测手势
+            _gestureRecognizer.GestureSettings = GestureSettings.Hold | GestureSettings.CrossSlide;
+            _gestureRecognizer.CrossSliding += (sender, args) =>
+            {
+                {
+                    lblMsg.Text += Environment.NewLine;
+                    lblMsg.Text += " CrossSliding: " + args.CrossSlidingState + "; " + args.Position.ToString();
+                }
+            };
+
+            _gestureRecognizer.Holding += (sender, args) =>
+            {
+                lblMsg.Text += Environment.NewLine;
+                lblMsg.Text += " Holding: " + args.HoldingState + "; " + args.Position.ToString();
+            };
+        }
+
+        private void radVertical_Click_1(object sender, Windows.UI.Xaml.RoutedEventArgs e)
+        {
+            // 监测垂直 Cross Slide 手势
+            _gestureRecognizer.CrossSlideHorizontally = false;
+        }
+
+        private void radHorizontal_Click_1(object sender, Windows.UI.Xaml.RoutedEventArgs e)
+        {
+            // 监测水平 Cross Slide 手势
+            _gestureRecognizer.CrossSlideHorizontally = true;
+        }
+
+        private void ButtonBase_OnClick(object sender, RoutedEventArgs e)
+        {
+            lblMsg.Text = "";
+        }
+    }
+}
